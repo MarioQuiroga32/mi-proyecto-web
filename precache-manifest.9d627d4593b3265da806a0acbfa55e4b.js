d0e553ce8b02bbc5771d05d556992148731211c1
Mario Quiroga <marioqui135@gmail.com>
2019-04-10 12:29:01 +0200

    Updates

---
+++ precache-manifest.9d627d4593b3265da806a0acbfa55e4b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "a0a6ca9794f3dc64cc68",
+    "url": "/static/css/main.0f5402e4.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9735d15598843bbdc1d8cc19c6a32ff9",
+    "url": "/static/media/background.9735d155.jpg"
+  },
+  {
+    "revision": "6ed6ccd2fa791fb4b3bd",
+    "url": "/static/js/2.b9227ac0.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "a0a6ca9794f3dc64cc68",
+    "url": "/static/js/main.4432a8b6.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "6ed6ccd2fa791fb4b3bd",
+    "url": "/static/css/2.6dbd973a.chunk.css"
+  },
+  {
+    "revision": "f44cb05c2173f948eafa4442a7add422",
+    "url": "/index.html"
+  }
+];
